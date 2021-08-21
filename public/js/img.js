6844d4062df1120891b63430343092bc2956495c
hurrykane0511 <86058971+hurrykane0511@users.noreply.github.com>
2021-08-21 17:44:20 +0700

    up

--- public/js/img.js
+++ public/js/img.js
@@ -1,6 +1,6 @@
 const navlinks = document.querySelectorAll('.nav-link');
 const nav_img = document.querySelectorAll('.img-wrap img');
-const img_tl = gsap.timeline();
+
 
 navlinks.forEach(link =>{
     link.addEventListener('mouseover', function(){
