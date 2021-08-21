f0ec6ba2eee80462b36134e654b94f3d616acc1a
hurrykane0511 <86058971+hurrykane0511@users.noreply.github.com>
2021-08-21 17:51:09 +0700

    Update img.js

--- public/js/img.js
+++ public/js/img.js
@@ -1,7 +1,6 @@
 const navlinks = document.querySelectorAll('.nav-link');
 const nav_img = document.querySelectorAll('.img-wrap img');
 
-
 navlinks.forEach(link =>{
     link.addEventListener('mouseover', function(){
         
